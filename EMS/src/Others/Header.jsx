c4e9984296a21d67312b8a038729129e0d556af2
Shibam <gshibam632@gmail.com>
2026-08-19 23:25:32 +0530

    Implemented Log Out Feature

--- EMS/src/Others/Header.jsx
+++ EMS/src/Others/Header.jsx
@@ -1,11 +1,16 @@
 
 const Header = () => {
+
+  const logOutUser = () => {
+    localStorage.setItem('loggedInUser','')
+    window.location.reload()
+  }
   return (
     <div className="flex items-end justify-between">
 
       <h1 className="text-2xl text-white font-medium">Hello <br/> <span className="text-3xl font-semibold">username 👋</span></h1>
 
-      <button className="bg-[#D90000] hover:bg-[#920101] active:scale-95 text-black text-lg px-5 py-2 rounded-xl">Log Out</button>
+      <button onClick={logOutUser}className="bg-[#D90000] hover:bg-[#920101] active:scale-95 text-black text-lg px-5 py-2 rounded-xl">Log Out</button>
     </div>
   )
 }
